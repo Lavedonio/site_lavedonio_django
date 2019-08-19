745e6e3d352cd7cfe85b5292c68b7feb53327cc8
Lavedonio <daniel.lavedonio@gmail.com>
2019-08-19 01:17:51 -0300

    Debug config for media files added

--- site_core/urls.py
+++ site_core/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls'))
+    path('', include('base.urls')),
+    path('blog/', include('blog.urls')),
+    path('projetos/', include('projects.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
