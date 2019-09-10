d989936a2705776dbd5a5c48b526d7ddeed392e9
Lavedonio <daniel.lavedonio@gmail.com>
2019-09-10 00:33:37 -0300

    Registered PostAdmin in Admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    '''
+        Admin View for Post
+    '''
+    list_display = ('title', 'subtitle', 'author', 'draft', 'date_posted', 'last_updated')
+    list_filter = ('draft', 'author')
+    search_fields = ('title',)
+
+admin.site.register(Post, PostAdmin)
