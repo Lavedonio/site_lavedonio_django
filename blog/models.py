2302d7bd9fbf1df4760bbc5b0a8bc3a0bc72da7f
Lavedonio <daniel.lavedonio@gmail.com>
2019-10-07 02:33:11 -0300

    2 new properties added. One to check the difference between date_posted and last_updated datetime; another for the image_name so to know which image to give credits to (default.jpg)

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
+import os
+import datetime
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -15,9 +18,21 @@ class Post(models.Model):
     last_updated = models.DateTimeField(auto_now=True, verbose_name=_('Last Updated'))
     content = models.TextField(verbose_name=_('Content'))
 
+    @property
+    def lt_one_minute_between_creation_and_update(self):
+        minutes = self.last_updated > self.date_posted + datetime.timedelta(minutes=1)
+        return minutes < 1
+
+    @property
+    def image_name(self):
+        return os.path.basename(self.main_image.name)
+
     class Meta:
         verbose_name = _('Post')
         verbose_name_plural = _('Posts')
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'pk': self.pk})
