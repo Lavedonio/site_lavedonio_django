fa23f4ebd6c8b0d0a46a6543b3179a3bc3dbf1b8
Lavedonio <daniel.lavedonio@gmail.com>
2019-10-09 02:51:13 -0300

    Added kwargs in all views and imported featured Posts and Projects to Homepage

--- base/views.py
+++ base/views.py
@@ -1,21 +1,26 @@
 from django.views.generic import TemplateView
+from blog.models import Post
+from projects.models import Project
 
 
 class HomepageView(TemplateView):
     template_name = "home.html"
 
-    def get_context_data(self):
-        context = super().get_context_data()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context["title"] = "Homepage"
         context["navbar_active"] = "home"
+
+        context["posts"] = Post.objects.filter(featured=True).order_by('-date_posted')[:3]
+        context["projects"] = Project.objects.filter(featured=True).order_by('-pk')[:3]
         return context
 
 
 class AboutView(TemplateView):
     template_name = "about.html"
 
-    def get_context_data(self):
-        context = super().get_context_data()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context["title"] = "Sobre"
         context["navbar_active"] = "about"
         return context
@@ -24,8 +29,8 @@ class AboutView(TemplateView):
 class ContactView(TemplateView):
     template_name = "contact.html"
 
-    def get_context_data(self):
-        context = super().get_context_data()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context["title"] = "Contato"
         context["navbar_active"] = "contact"
         return context
