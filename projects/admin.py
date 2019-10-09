42cd7214b093f80cac8efb9f0a2d0e7af67a0326
Lavedonio <daniel.lavedonio@gmail.com>
2019-10-09 02:55:48 -0300

    Added Featured Field to Admin page

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    '''
+        Admin View for Project
+    '''
+    list_display = ('title', 'slug', 'featured', 'description', 'dark_background', 'highest_score', 'downloads')
+    list_filter = ('featured', 'dark_background')
+    search_fields = ('title', 'slug')
+
+admin.site.register(Project, ProjectAdmin)
