4c6b276a7aad016b120a3c69299cffed51986851
Lavedonio <daniel.lavedonio@gmail.com>
2019-10-09 02:54:23 -0300

    Added featured field to models

--- projects/models.py
+++ projects/models.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class Project(models.Model):
     title = models.CharField(max_length=64, verbose_name=_('Title'))
     slug = models.SlugField(max_length=32, unique=True, verbose_name=_('Slug'))
+    featured = models.BooleanField(default=False, verbose_name=_('Featured'))
     description = models.CharField(max_length=128, verbose_name=_('Description'))
     dark_background = models.BooleanField(default=False, verbose_name=_('Dark Background'))
     highest_score = models.IntegerField(default=0, verbose_name=_('Highest Score'))
