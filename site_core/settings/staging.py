4035083fffb3a24109eddd7607d73e8f4be374e6
Lavedonio <daniel.lavedonio@gmail.com>
2020-03-04 03:25:06 +0000

    Reconfigured staging settings so it works with POSTGRESQL

--- site_core/settings/staging.py
+++ site_core/settings/staging.py
@@ -36,12 +36,12 @@ ALLOWED_HOSTS = ['45.79.220.173']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config["DB_NAME"],
         'USER': config["DB_USER"],
         'PASSWORD': config["DB_PASS"],
         'HOST': "localhost",
-        'PORT': "5432",
+        'PORT': "",
     }
 }
 
