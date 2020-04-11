ad8bea6e13f8712df0b11ed15c0b150f3335deab
Lavedonio <daniel.lavedonio@gmail.com>
2020-04-11 20:21:59 -0300

    New custom save method reduces image size before saving it

--- file_tools/models.py
+++ file_tools/models.py
@@ -2,6 +2,10 @@ import uuid
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from PIL import Image
+from io import BytesIO
 
 
 class FileUpload(models.Model):
@@ -37,8 +41,52 @@ class ImageUpload(models.Model):
         verbose_name = _("Image Upload")
         verbose_name_plural = _("Image Uploads")
 
+    # This idea was adapted from:
+    # https://stackoverflow.com/questions/1355150/when-saving-how-can-you-check-if-a-field-has-changed/13842223#13842223
+    __previous_image_upload = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__previous_image_upload = self.image_upload
+
     def __str__(self):
         return str(self.id)
 
     def get_absolute_url(self):
         return reverse('image', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        # If image has changed, compress it and save.
+        if self.image_upload != self.__previous_image_upload:
+            with Image.open(BytesIO(self.image_upload.read())) as img:
+
+                # Convert to RGB
+                if img.mode != 'RGB':
+                    img = img.convert('RGB')
+
+                # Resizing
+                size = (800, 800)
+                img.thumbnail(size, Image.ANTIALIAS)
+
+                # Reattributing resized file
+                with BytesIO() as output:
+
+                    # Outputing saved file
+                    img.save(output, format='JPEG', quality=90)
+                    output.seek(0)
+
+                    # Replacing image instance
+                    self.image_upload = InMemoryUploadedFile(
+                        output, 'ImageField',
+                        "%s.jpg" % self.image_upload.name.split('.')[0],
+                        'image/jpeg',
+                        output.getbuffer().nbytes, None
+                    )
+
+                    # Complete instance saving
+                    super().save(*args, **kwargs)
+
+        # If the image hasn't changed, save normally and update self.__previous_image_upload variable
+        else:
+            super().save(*args, **kwargs)
+            self.__previous_image_upload = self.image_upload
