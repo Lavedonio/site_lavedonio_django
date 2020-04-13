cf951840f72f8ebfbfe3e5e2965583bbb3fbaf60
Lavedonio <daniel.lavedonio@gmail.com>
2020-04-13 02:47:32 -0300

    Setting DEBUG to False

--- site_core/settings/staging.py
+++ site_core/settings/staging.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = config["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['staging.lavedonio.com.br', '45.79.220.173']
 
