dfdd96d4bd0fda2551726831a805d0d2e7118365
Lavedonio <daniel.lavedonio@gmail.com>
2020-04-22 02:35:40 -0300

    Changed if/else order for choosing environment type. Now defaults to development

--- site_core/settings/__init__.py
+++ site_core/settings/__init__.py
@@ -4,7 +4,7 @@ from .config import config
 # Imports base settings and environment specific settings
 from .base import *
 
-if config["ENVIRONMENT"] == "development":
-    from .development import *
-else:
+if config["ENVIRONMENT"] == "staging":
     from .staging import *
+else:
+    from .development import *
