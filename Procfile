03565e5e89a7194b216eba6f55444b40d7366ad9
Lavedonio <daniel.lavedonio@gmail.com>
2020-04-23 11:18:46 -0300

    Added bash command before sh file

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: ./release-tasks.sh
+release: bash ./release-tasks.sh
 web: gunicorn site_core.wsgi
--- release-tasks.sh
+++ release-tasks.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 python manage.py makemigrations
 python manage.py migrate
 django-admin compilemessages --noinput
