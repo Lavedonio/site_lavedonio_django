48ceafe7d5fed3fc5634c5846f4658acbe606b6b
Lavedonio <daniel.lavedonio@gmail.com>
2020-04-22 22:56:25 -0300

    Added base settings to development app too

--- site_core/settings/development.py
+++ site_core/settings/development.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from .config import config
+from .base import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
