dfb853365f05f97b3df9744611f021016055b2ec
Lavedonio <daniel.lavedonio@gmail.com>
2020-04-25 21:09:08 -0300

    Reorganized variables so it becomes easier to read an maintain

--- site_core/settings/secrets_example.py
+++ site_core/settings/secrets_example.py
@@ -1,28 +1,39 @@
-#######################################################
-#                                                     #
-#          Place sensitive information here           #
-#                                                     #
-# ATTENTION: don't edit this file. Copy and rename it #
-#            to 'config.py' to set the configuration. #
-#                                                     #
-#######################################################
+########################################################
+#                                                      #
+#           Place sensitive information here           #
+#                                                      #
+# ATTENTION: don't edit this file. Copy and rename it  #
+#            to 'secrets.py' to set the configuration. #
+#                                                      #
+########################################################
 
 
 secrets = {
+    #Django
     "ENVIRONMENT": "development",
     "SECRET_KEY": "",
     "DEBUG_VALUE": "True",
     "SERVER_TYPE": "",
+
+    # E-mail
+    "EMAIL_USER": "",
+    "EMAIL_PASS": "",
+    "HTTPS_TRAFFIC_ONLY": "",
+
+    # RECAPTCHA
+    "RECAPTCHA_PUBLIC_KEY": "",
+    "RECAPTCHA_PRIVATE_KEY": "",
+
+    # Postgres Database
     "DB_NAME": "",
     "DB_USER": "",
     "DB_PASS": "",
-    "RECAPTCHA_PUBLIC_KEY": "",
-    "RECAPTCHA_PRIVATE_KEY": "",
-    "EMAIL_USER": "",
-    "EMAIL_PASS": "",
+
+    # AWS
     "AWS_ACCESS_KEY_ID": "",
     "AWS_SECRET_ACCESS_KEY": "",
     "AWS_STORAGE_BUCKET_NAME": "",
-    "HTTPS_TRAFFIC_ONLY": "",
+
+    # Google Analytics
     "ANALYTICS_ID": ""
 }
